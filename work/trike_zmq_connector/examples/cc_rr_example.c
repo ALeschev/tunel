de87ab696c168ab962cf5973cea5758f5a871fcb
Alexander Leschev <alexander.leschev@eltex.loc>
2014-06-23 19:49:23 +0400

    zmq+asn: improved server/client

--- work/trike_zmq_connector/examples/cc_rr_example.c
+++ work/trike_zmq_connector/examples/cc_rr_example.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "PongType.h"
+
 #include "trike_zmq_worker_api.h"
 #include "trike_msg_api.h"
 #include "trike_gen_cc_rr.h"
@@ -17,40 +19,95 @@ volatile int count=0;
 void init_cb(void);
 
 int main(){
-	trike_worker_context_t *worker_context=start_worker(init_cb);
-	trike_transport_context_t *client_context;
-	char bind_addr[]="tcp://*";
-	char addr[]="tcp://127.0.0.1";
-	char data[]="asd";
+    trike_worker_context_t *worker_context=start_worker(init_cb);
+    trike_transport_context_t *client_context;
+    char bind_addr[]="tcp://*";
+    char addr[]="tcp://127.0.0.1";
+    char data[]="ping";
         char buf[MAX_SIZE];
         trike_cc_rr_msg_t trike_cc_rr_msg;
         int i;
-	add_rr_server(worker_context,make_rr_reply(.handle_request=handle_request,
+
+/*  add_rr_server(worker_context,make_rr_reply(.handle_request=handle_request,
                                                    .req_port=50500,
                                                    .rep_port=50502,
-                                                   .addr=bind_addr));
-	client_context=add_rr_client(worker_context,make_rr_request(.handle_reply=handle_reply,
+                                                   .addr=bind_addr));*/
+    client_context=add_rr_client(worker_context,make_rr_request(.handle_reply=handle_reply,
                                                                     .req_port=50500,
                                                                     .rep_port=50502,
                                                                     .addr=addr));
-	printf("Started receive-response example\n");
-	if(!client_context) {printf("Sorry, error happens\n"); return 0;}
-	printf("Try to send requests to server\n");
+    printf("Started receive-response example\n");
+    if(!client_context) {printf("Sorry, error happens\n"); return 0;}
+    printf("Try to send requests to server\n");
+
         for(i=0;i<MESSAGE_COUNT;i++){
-            snprintf(buf,MAX_SIZE,"%s_%i",data,i);
-            trike_cc_rr_msg.dialog_id_data=buf;
-            trike_cc_rr_msg.dialog_id_size=sizeof(buf);
-            trike_cc_rr_msg.request_id=i;
-            trike_cc_rr_msg.size=strlen(buf);
-            trike_cc_rr_msg.data=buf;
-            trike_send_cc_rr_msg(client_context,trike_cc_rr_msg);
+            //snprintf(buf,MAX_SIZE,"%s_%i",data,i);
+            int type_size = sizeof(PongType_t);
+            char buffer[sizeof(PongType_t)] = {0};
+            //void *buffer = 0;
+            PongType_t *c_pong;
+
+            c_pong = calloc(1, type_size);
+            assert(c_pong);
+
+            OCTET_STRING_fromBuf (&c_pong->buff, "ping", 4);
+
+            xer_fprint(stdout, &asn_DEF_PongType, c_pong);
+
+// ssize_t uper_encode_to_new_buffer(
+//     struct asn_TYPE_descriptor_s *type_descriptor,
+//     asn_per_constraints_t *constraints,
+//     void *struct_ptr,   /* Structure to be encoded 
+//     void **buffer_r     /* Buffer allocated and returned */
+// );
+
+        //uper_encode_to_buffer(asn_TYPE_descriptor_t *td, void *sptr, void *buffer, size_t buffer_size)
+        asn_enc_rval_t rvalue;
+        rvalue = uper_encode_to_buffer (&asn_DEF_PongType, c_pong, buffer, type_size);
+
+        if (rvalue.encoded < 0)
+        {
+            printf ("%d endcode failed\n", i);
+            continue;
         }
-	WAIT_COUNTER(count,MESSAGE_COUNT);
+
+        trike_cc_rr_msg.dialog_id_data="42";
+        trike_cc_rr_msg.dialog_id_size=2;
+        trike_cc_rr_msg.request_id=i;
+        trike_cc_rr_msg.size=type_size;
+        trike_cc_rr_msg.data=buffer;
+
+        PongType_t *p_pong;
+        asn_dec_rval_t rval;
+
+        rval = uper_decode_complete(0, &asn_DEF_PongType, (void **)&p_pong, buffer, type_size);
+        assert (p_pong);
+
+        switch(rval.code) {
+            case RC_OK:
+                printf ("uper_decode_complete: RC_OK\n");
+                break;
+            case RC_FAIL:
+                printf ("uper_decode_complete: RC_FAIL\n");
+                break;
+            case RC_WMORE:
+                printf ("uper_decode_complete: RC_WMORE\n");
+                break;
+            }
+
+            trike_send_cc_rr_msg(client_context,trike_cc_rr_msg);
+
+            FREEMEM(buffer);
+
+            free(c_pong);
+        }
+
+    WAIT_COUNTER(count,MESSAGE_COUNT);
         printf("All message sended.");
-	return 0;
+    return 0;
 }
 
-int handle_request(trike_transport_context_t *transport_context,trike_cc_rr_msg_t *msg){
+/*int handle_request(trike_transport_context_t *transport_context,trike_cc_rr_msg_t *msg){
     char buf[]="response";
     trike_cc_rr_msg_t trike_cc_rr_msg;
     trike_cc_rr_msg.dialog_id_data=msg->dialog_id_data;
@@ -59,14 +116,29 @@ int handle_request(trike_transport_context_t *transport_context,trike_cc_rr_msg_
     trike_cc_rr_msg.size=sizeof(buf);
     trike_cc_rr_msg.data=buf;
     printf("Received request with id=%i: %s\n",msg->request_id,(char*)msg->data);
+
+    printf("Received request:\n"
+           "msg->dialog_id_data: %s\n"
+           "msg->dialog_id_size: %d\n"
+           "msg->request_id: %d\n"
+           "msg->size: %d\n"
+           "msg->data: %s\n",
+           (char *)&msg->dialog_id_data,
+           msg->dialog_id_size,
+           msg->request_id,
+           msg->size,
+           (char*)msg->data
+           );
+
+
     trike_send_cc_rr_msg(transport_context,trike_cc_rr_msg);
     return 0;
-}
+}*/
 
 int handle_reply(trike_transport_context_t *transport_context,trike_cc_rr_msg_t *msg){
     char response[msg->size];
     memcpy(response,msg->data,msg->size);
-    printf("Received response with request_id=%i: %s\n",msg->request_id,response);
+    printf("\nReceived response with request_id=%i: %s\n",msg->request_id, response);
     count++;
     return 0;
 }
--- work/trike_zmq_connector/examples/zmq_pong_server.c
+++ work/trike_zmq_connector/examples/zmq_pong_server.c
@@ -6,9 +6,15 @@
 #include "trike_msg_api.h"
 #include "trike_gen_cc_rr.h"
 
+//#define ERL_TEST 1
+
+#ifndef ERL_TEST
+#	include "PongType.h"
+#endif
+
 int handle_request(trike_transport_context_t *transport_context, trike_cc_rr_msg_t *msg);
 
-int c = 10;
+int c = 100;
 
 int main()
 {
@@ -42,58 +48,103 @@ int main()
 
 int handle_request(trike_transport_context_t *transport_context, trike_cc_rr_msg_t *msg)
 {
-	char err_ans[] = "err";
-	char pong_ans[] = "pong";
-
-	char *buff;
+	int res;
 
 	trike_cc_rr_msg_t trike_cc_rr_msg;
 
-	memcpy (&trike_cc_rr_msg, msg, sizeof (trike_cc_rr_msg_t));
-
-/*	trike_cc_rr_msg.dialog_id_data = msg->dialog_id_data;
+	trike_cc_rr_msg.dialog_id_data = msg->dialog_id_data;
 	trike_cc_rr_msg.dialog_id_size = msg->dialog_id_size;
 	trike_cc_rr_msg.request_id     = msg->request_id;
 
-	trike_cc_rr_msg.size           = sizeof(err_ans);
-	trike_cc_rr_msg.data           = err_ans;*/
+#ifdef ERL_TEST
 
-	// if (strcmp((char*)msg->data, "pin"))
-	// {
-	// 	trike_cc_rr_msg.size           = sizeof(err_ans);
-	// 	trike_cc_rr_msg.data           = err_ans;
-	// } else {
-	// 	trike_cc_rr_msg.size           = sizeof(pong_ans);
-	// 	trike_cc_rr_msg.data           = pong_ans;
-	// }
+	char err_ans[] = "err";
+	char pong_ans[] = "pong";
 
-//	printf("Received request with id=%i: %s\n", msg->request_id, (char*)msg->data);
+	char buff[256] = {0};
 
-    printf("Received request:\n"
+	printf("Received request:\n"
            "msg->dialog_id_data: %s\n"
            "msg->dialog_id_size: %d\n"
            "msg->request_id: %d\n"
+           "msg->size: %d\n"
            "msg->data: %s\n",
            (char *)msg->dialog_id_data,
            msg->dialog_id_size,
            msg->request_id,
+           msg->size,
            (char*)msg->data
            );
 
-    printf("Send answer:\n"
+	trike_cc_rr_msg.size = sizeof(err_ans) - 1;
+	trike_cc_rr_msg.data = err_ans;
+
+	if (msg->size < 256)
+	{
+		memcpy (buff, msg->data, msg->size);
+	} else {
+		printf ("invalid msg size: %d\n", msg->size);
+		goto send;
+	}
+
+	if (!strcmp(buff, "ping"))
+	{
+		trike_cc_rr_msg.size = sizeof(pong_ans) - 1;
+		trike_cc_rr_msg.data = pong_ans;
+	}
+
+	printf("\nSend answer:\n"
            "msg->dialog_id_data: %s\n"
            "msg->dialog_id_size: %d\n"
            "msg->request_id: %d\n"
+           "msg->size: %d\n"
            "msg->data: %s\n",
            (char *)trike_cc_rr_msg.dialog_id_data,
            trike_cc_rr_msg.dialog_id_size,
            trike_cc_rr_msg.request_id,
+           trike_cc_rr_msg.size,
            (char*)trike_cc_rr_msg.data
            );
 
-	trike_send_cc_rr_msg(transport_context, trike_cc_rr_msg);
+	printf ("--------------------\n");
 
-//	printf("Send answer with id=%i: %s\n", msg->request_id, (char *)trike_cc_rr_msg.data);
+#else
+		asn_dec_rval_t rval;
+		PongType_t *c_pong;
+
+	printf("Received request:\n"
+           "msg->dialog_id_data: %s\n"
+           "msg->dialog_id_size: %d\n"
+           "msg->request_id: %d\n"
+           "msg->size: %d\n",
+           (char *)msg->dialog_id_data,
+           msg->dialog_id_size,
+           msg->request_id,
+           msg->size);
+
+		//uper_decode(asn_codec_ctx_t *opt_codec_ctx, asn_TYPE_descriptor_t *td, void **sptr, const void *buffer, size_t size, int skip_bits, int unused_bits)
+		rval = uper_decode_complete(0, &asn_DEF_PongType, (void **)&c_pong, msg->data, msg->size);
+		assert(c_pong);
+
+		switch(rval.code) {
+			case RC_OK:
+				printf ("uper_decode_complete: RC_OK\n");
+				break;
+			case RC_FAIL:
+				printf ("uper_decode_complete: RC_FAIL\n");
+				return 0;
+			case RC_WMORE:
+				printf ("uper_decode_complete: RC_WMORE\n");
+				return 0;
+			}
+
+		xer_fprint(stdout, &asn_DEF_PongType, c_pong);
+#endif
+
+send:
+	res = trike_send_cc_rr_msg(transport_context, trike_cc_rr_msg);
+
+	printf("Send message %s!\n", res? "failed":"ok");
 
 	--c;
 
