862c8d526a0edff5ffbc944b637fad063e1e19d6
alexander.leschev <alexander.leschev@eltex.loc>
2014-12-05 23:02:19 +0700

    pvt: improved parallel source

--- sibsutis/2014/pvt/1/main_pth.c
+++ sibsutis/2014/pvt/1/main_pth.c
@@ -6,6 +6,18 @@
 #include <errno.h>
 #include "hpctimer.h"
 
+#if defined(THREAD_2)
+#define MAX_THREAD 2
+#elif defined(THREAD_4)
+#define MAX_THREAD 4
+#elif defined(THREAD_6)
+#define MAX_THREAD 6
+#elif defined(THREAD_8)
+#define MAX_THREAD 8
+#else
+#error Unsupported thread count.
+#endif
+
 enum th_state { DISABLE = 0, ENABLE };
 enum e_state { EMPTY = 0 };
 
@@ -63,25 +75,49 @@ void *pth_step(void *arg)
 
 void sieve_of_Eratosthenes()
 {
-    int i;
-    pthread_t th_id[2];
-    struct thread_data th_data[2];
+    int i, step, th_num = 0;
+    pthread_t th_id[MAX_THREAD];
+    struct thread_data th_data[MAX_THREAD];
+    struct thread_data *p_data;
 
-    for (i = 0; i < max; i++)
-        arr[i] = i;
+    step = max/MAX_THREAD;
+    p_data = th_data;
+    for (i = 0; i < max; i += step)
+    {
+        p_data->th_num = th_num;
+        p_data->start = i;
+        p_data->end = i + step;
 
-    th_data[0].th_num = 0;
+#ifdef DEBUG
+        printf("Thread %d inited: %d -> %d\n", th_num, p_data->start, p_data->end);
+#endif
+
+        th_num++;
+        p_data++;
+    }
+
+/*    th_data[0].th_num = 0;
     th_data[0].start = 0;
     th_data[0].end = max/2;
     th_data[1].th_num = 1;
     th_data[1].start = max/2 + 1;
     th_data[1].end = max-1;
+*/
 
-    pthread_create (&th_id[0], NULL, pth_step, (void *)&th_data[0]);
-    pthread_create (&th_id[1], NULL, pth_step, (void *)&th_data[1]);
+    int res;
+    for (i = 0; i < th_num; i++)
+    {
+        res = pthread_create (&th_id[i], NULL, pth_step, (void *)&th_data[i]);
+        if (res)
+            printf ("Tread %d start failed: %s", th_data[i].th_num, strerror(errno));
+    }
 
-    pthread_join (th_id[0], NULL);
-    pthread_join (th_id[1], NULL);
+    for (i = 0; i < th_num; i++)
+    {
+        res = pthread_join (th_id[i], NULL);
+        if (res)
+            printf ("Tread %d join failed: %s", th_data[i].th_num, strerror(errno));
+    }
 
     int snum = 0;
     for (i = 0; i < max; i++)
@@ -97,6 +133,7 @@ void sieve_of_Eratosthenes()
 
 int main (int argc, char *argv[])
 {
+    int i;
     double t;
 
     if (argc < 2)
@@ -114,6 +151,13 @@ int main (int argc, char *argv[])
 
     if (arr_init() == NULL)
         return 1;
+
+    for (i = 0; i < max; i++)
+        arr[i] = i;
+
+    printf ("Sieve of Eratosthenes started.\n"
+            "Range: 0 - %d.\n"
+            "Total threads %d\n", max, MAX_THREAD);
 
     t = hpctimer_wtime();
 
